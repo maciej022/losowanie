88093d2796ec1ac940cab9996c93c9656a446356
student <student@STUDENT-5>
2025-02-11 11:28:01 +0100

    Dodaj pliki projektów.

---
+++ WpfApp15/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp15
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void zatwierdzbtn_Click(object sender, RoutedEventArgs e)
+        {
+            int dlugosc;
+            if (!int.TryParse(ileznakowtxt.Text, out dlugosc))
+            {
+                MessageBox.Show("Podaj dlugosc");
+                return;
+            }
+            string litery = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+            string liczby = "1234567890";
+            string specjalne="!@#$%^&*()-_+"
+            List<char> list = new List<char>();
+        }
+    }
+}
